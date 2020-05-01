d66c53abbfc863ea76a6e7e6fdf3f2cd334a2151
Mita57 <57thrasher57@gmail.com>
2020-05-01 14:37:03 +0500

    kyu

--- server/models/BasicModel.py
+++ server/models/BasicModel.py
@@ -2,46 +2,7 @@ from contextlib import closing
 import psycopg2
 
 
-class AbstractClassError(Exception):
-    pass
-
-
 class SQLModel:
-    _DATABASE = 'bakery'
-    _USER = 'postgres'
-    _PASSWORD = 'postgres'
-    _HOST = 'localhost'
-    _PORT = '5432'
-
-    def __init__(self):
-        raise AbstractClassError
-
-    @classmethod
-    def _connect(cls):
-        """
-        connects to the POSTGRESQL batabase
-        Returns:
-        Connection to postgres
-        """
-        return psycopg2.connect(database=cls._DATABASE,  user=cls._USER,
-                                password=cls._PASSWORD, host=cls._HOST, port=cls._PORT)
-
-    @classmethod
-    def query(cls, query, attrs=None):
-        """
-        Performs a query than us not specified in the other methods
-        Args:
-            query; a query to be executed
-            attrs: a set of attributes, if needed of the query
-        """
-        conn = cls._connect(cls._DATABASE)
-        cur = conn.cursor()
-        if attrs:
-            cur.execute(query, attrs)
-        else:
-            cur.execute(query)
-        conn.commit()
-        conn.close()
 
     @classmethod
     def normalize_cols(cls, cols):
@@ -51,89 +12,67 @@ class SQLModel:
         new_cols = new_cols[0:-2]
         return new_cols
 
-    @classmethod
-    def insert(cls, values):
+    @staticmethod
+    def insert(table, cols, values):
         """
             Inserts the data into the database
             args:
                 values: the values to be inserted into the database
         """
-        with closing(psycopg2.connect(database='streamoor', user='postgres', password='3395925000',
+        with closing(psycopg2.connect(database='crm', user='postgres', password='MOORMOOR',
                                       host='127.0.0.1', port='5432')) as conn:
             with conn.cursor() as cursor:
-                sql_insert_query = """INSERT INTO {} VALUES {} """.format(cls._TABLE, values)
+                sql_insert_query = """INSERT INTO {}({}) VALUES {} """.format(table, cols, values)
+                print(sql_insert_query)
                 cursor.execute(sql_insert_query)
                 conn.commit()
 
     @classmethod
-    def get_by_attrs(cls, cols, attr_cols, attr_values, group_by=None, order_by=None):
-        """
-            Gets the values from the database that correspond to the values given
-            attrs:
-                cols: columns that will be returned : list
-                attr_cols: columns that will be used in the WHERE statement : list
-                attr_values: values that will be used in the WHERE statement according to the attr_cols : list
-                group_by: set None as default, if it is given, then the query result is grouped by this arg
-                sorted_by: set None as defaul, if it si given, then they query result is sorted by this arg
-            returns:
-                query result as a dictionary
-        """
+    def get_by_attrs(cls, cols, table, attr_cols, attr_values, group_by=None, order_by=None):
         new_cols = cls.normalize_cols(cols)
-        with closing(psycopg2.connect(database='streamoor', user='postgres', password='3395925000',
+        with closing(psycopg2.connect(database='crm', user='postgres', password='MOORMOOR',
                                       host='127.0.0.1', port='5432')) as conn:
             with conn.cursor() as cursor:
                 if group_by is None and order_by is None:
-                    sql_select_query = """SELECT {} FROM {} WHERE {}='{}' """.format(new_cols, cls._TABLE, attr_cols, attr_values)
+                    sql_select_query = """SELECT {} FROM {} WHERE {}='{}' """.format(new_cols, table, attr_cols, attr_values)
                     cursor.execute(sql_select_query)
                     value = cursor.fetchall()
                     return value
                 if group_by is not None and order_by is None:
-                    sql_select_query = """SELECT {} FROM {} WHERE {}='{}' GROUP BY {}""".format(new_cols, cls._TABLE, attr_cols, attr_values, group_by)
+                    sql_select_query = """SELECT {} FROM {} WHERE {}='{}' GROUP BY {}""".format(new_cols, table, attr_cols, attr_values, group_by)
                     cursor.execute(sql_select_query)
                     value = cursor.fetchall()
                     return value
                 if group_by is None and order_by is not None:
-                    sql_select_query = ("""SELECT {} FROM {} WHERE {} = '{}' ORDER BY {}""").format(new_cols, cls._TABLE, attr_cols, attr_values, order_by)
+                    sql_select_query = ("""SELECT {} FROM {} WHERE {} = '{}' ORDER BY {}""").format(new_cols, table, attr_cols, attr_values, order_by)
                     cursor.execute(sql_select_query)
                     value = cursor.fetchall()
                     return value
                 else:
                     sql_select_query = """SELECT {} FROM {} WHERE {}='{}' GROUP BY {} ORDER BY {}""".format(
-                        new_cols, cls._TABLE, attr_cols, attr_values, group_by, order_by)
+                        new_cols, table, attr_cols, attr_values, group_by, order_by)
                     cursor.execute(sql_select_query)
                     value = cursor.fectchall()
                     return value
-
     @classmethod
-    def update_by_attrs(cls, columns, values, attr_cols, attr_values):
-        """
-            Updates the values in the database that correspond to the values given
-            attrs:
-                columns: columns that will be updated : list
-                values: values that will be inserted into the DB in accordance to columns : list
-                attr_cols: columns that will be used in the WHERE statement : list
-                attr_values: values that will be used in the WHERE statement according to the attr_cols : list
-        """
-        new_cols = cls.normalize_cols(columns)
-        with closing(psycopg2.connect(database='streamoor', user='postgres', password='3395925000',
+    def update_by_attrs(cls, table, columns, values, attr_cols, attr_values):
+        with closing(psycopg2.connect(database='crm', user='postgres', password='MOORMOOR',
                                       host='127.0.0.1', port='5432')) as conn:
             with conn.cursor() as cursor:
-                sql_update_query = """UPDATE {} SET {}=%s WHERE {}=%s""".format(cls._TABLE, new_cols, attr_cols)
-                cursor.execute(sql_update_query, (values, attr_values))
+                sql_update_query = """UPDATE {} SET ({})={} WHERE {}={}""".format(table, columns, values, attr_cols, attr_values)
+                print(sql_update_query)
+                cursor.execute(sql_update_query)
+                conn.commit()
 
-    @classmethod
-    def delete_by_attrs(cls, attr_cols, attr_values):
-        """
-            Deletes the rows in the database that correspond to the values given
-            attrs:
-                attr_cols: columns that will be used in the WHERE statement : list
-                attr_values: values that will be used in the WHERE statement according to the attr_cols : list
-        """
-        with closing(psycopg2.connect(database='streamoor', user='postgres', password='3395925000',
+    @staticmethod
+    def delete_by_attrs(table, attr_cols, attr_values):
+        with closing(psycopg2.connect(database='crm', user='postgres', password='MOORMOOR',
                                       host='127.0.0.1', port='5432')) as conn:
             with conn.cursor() as cursor:
-                sql_delete_query = """DELETE FROM {} where {}=%s""".format(cls._TABLE, attr_cols)
-                cursor.execute(sql_delete_query, attr_values)
+                sql_delete_query = """DELETE FROM {} where {}='{}'""".format(table, attr_cols, attr_values)
+                print(sql_delete_query)
+                cursor.execute(sql_delete_query)
+                conn.commit()
 
 
 class BasicModel(SQLModel):
