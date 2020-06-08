d493d1a7ae655d83406648ed3e6627c10f7b9a4d
Mita57 <57thrasher57@gmail.com>
2020-06-09 01:52:43 +0500

    FEWE

--- client/src/views/Profile.vue
+++ client/src/views/Profile.vue
@@ -1,10 +1,32 @@
 <template>
-
+    <v-container class="grey lighten-5">
+        <v-row no-gutters>
+            <v-col cols="12" sm="6" md="8" >
+                <v-card class="mx-auto" outlined>
+                    <v-list-item three-line>
+                        <div class="text-center">
+                            <v-img :src="require('../assets/bready.jpg')" max-width="125"/>
+                            <v-btn x-small color="success" dark>Поменять аватар</v-btn>
+                        </div>
+                        <v-list-item-content class="ml-5">
+                            <v-list-item-title class="headline mb-1">Username</v-list-item-title>
+                            <v-list-item-subtitle>Info</v-list-item-subtitle>
+                        </v-list-item-content>
+                    </v-list-item>
+                    <v-card-actions>
+                    </v-card-actions>
+                </v-card>
+            </v-col>
+        </v-row>
+    </v-container>
 </template>
 
 <script>
     export default {
-        name: "Profile"
+        name: "Profile",
+        mounted() {
+            document.title = "Профиль";
+        }
     }
 </script>
 
